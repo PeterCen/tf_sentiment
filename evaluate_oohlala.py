8f11457b6578f0fadf18569a4663bc3202b82ea0
Peter Cen <peter.cen@gmail.com>
2017-05-24 17:15:04 -0400

    changed output

--- evaluate_oohlala.py
+++ evaluate_oohlala.py
@@ -59,8 +59,10 @@ def main():
 			print output_feed
 			outputs = sess.run(output_feed, input_feed)
 			score = np.argmax(outputs[0])
+			score1 = np.argmax(outputs[1])
+			score2 = np.argmax(outputs[2])
 			probability = outputs[0].max(axis=1)[0]
-			print "Value of sentiment: {0} with probability: {1}".format(score , probability)
+			print "Value of sentiment: {0} {1} {2} with probability: {3}".format(score, score1, score2, probability)
 			output_row = [text, score, probability]
 			output_row.extend(tokenize(text))
 			output_data.append(output_row)
